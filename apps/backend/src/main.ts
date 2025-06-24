1e39f742ca2be557b88fb462c5a9fe94fff5d228
glenn-syj <glenn.syj@gmail.com>
2025-06-24 15:30:13 +0900

    feat: API 경로 접두사 설정 추가
    - `main.ts` 파일에서 글로벌 API 경로 접두사를 'api'로 설정하여 API 엔드포인트를 명확히 구분함.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
+  app.setGlobalPrefix('api');
 
   app.enableCors({
     origin: 'http://localhost:3001', // Next.js 개발 서버 주소
